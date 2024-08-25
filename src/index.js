8574f7a494df813e21db5ac6833c63cdf761373c
bhlbrian <bhlbrianhumacata@gmail.com>
2024-08-25 18:11:34 -0400

    404

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import { Provider } from'react-redux';
 import { RouterProvider } from 'react-router-dom'
 import routes from './router/router';
 
-const basename = process.env.NODE_ENV === 'production' ? '/desarrollo-frontend-react' : '/';
-
+const basename = '/desarrollo-frontend-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
